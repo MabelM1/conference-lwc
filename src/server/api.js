52ce842fc628daf269cc3d956460201bfe08e0f0
Mabel <macmota@hotmail.com>
2021-02-04 20:02:54 -0500

    update package.json

--- src/server/api.js
+++ src/server/api.js
@@ -2,6 +2,7 @@
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(helmet());
@@ -9,6 +10,13 @@ app.use(compression());
 
 const HOST = process.env.API_HOST || 'localhost';
 const PORT = process.env.API_PORT || 3002;
+const DIST_DIR = './dist';
+
+app.use(express.static(DIST_DIR));
+
+app.use(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
 
 app.get('/api/v1/endpoint', (req, res) => {
     res.json({ success: true });
